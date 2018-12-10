b25bd70d1da1a84692a9c563800d000838d1a487
Mikael Beyene <mikael.beyene@dscsag.com>
2018-12-10 16:49:02 +0100

    Change Spring setup

--- src/main/java/org/beyene/zmq/Application.java
+++ src/main/java/org/beyene/zmq/Application.java
@@ -1,10 +1,19 @@
+
 package org.beyene.zmq;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 
-public class Application {
+public class Application
+{
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args)
+  {
+    SpringApplication application = new SpringApplication(Application.class);
+    application.setWebApplicationType(WebApplicationType.NONE);
+
+    // Manually register bean that provides ZMQ context (and closes it)
+    //      application.addInitializers((GenericApplicationContext ctx) -> ctx.registerBean(TypeClass.class, () -> typeObject));
+    application.run(args);
+  }
 }
