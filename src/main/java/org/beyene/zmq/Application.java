127a8327adf4f25a79b7a1fa37a62c26aa6b1b2a
Mikael Beyene <mikael@beyene.org>
2018-12-10 14:42:42 +0100

    Add Application.java

---
+++ src/main/java/org/beyene/zmq/Application.java
@@ -0,0 +1,10 @@
+package org.beyene.zmq;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
